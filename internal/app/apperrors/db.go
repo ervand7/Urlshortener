46a237b8ecc3fcb14a3fcb35672bd2b35b5032cc
ervand7 <ervand7@mail.ru>
2022-09-14 21:12:08 +0300

    increment13_

---
+++ internal/app/apperrors/db.go
@@ -0,0 +1,24 @@
+package apperrors
+
+import (
+	"errors"
+	"fmt"
+)
+
+type ShortAlreadyExistsError struct {
+	Err error
+}
+
+func NewShortAlreadyExistsError(short string) error {
+	return &ShortAlreadyExistsError{
+		Err: fmt.Errorf(`%w`, errors.New(short)),
+	}
+}
+
+func (s ShortAlreadyExistsError) Unwrap() error {
+	return s.Err
+}
+
+func (s ShortAlreadyExistsError) Error() string {
+	return fmt.Sprintf("%s", s.Err)
+}
--- internal/app/views/url_test.go
+++ internal/app/views/url_test.go
@@ -461,3 +461,98 @@ func TestURLShortenBatch(t *testing.T) {
 		})
 	}
 }
+
+func Test409(t *testing.T) {
+	if os.Getenv("DATABASE_DSN") != "user=ervand password=ervand dbname=urlshortener_test host=localhost port=5432 sslmode=disable" {
+		return
+	}
+
+	type want struct {
+		contentType string
+	}
+	tests := []struct {
+		name     string
+		handler  string
+		endpoint string
+		body     string
+		method   string
+		want     want
+	}{
+		{
+			name:     "success 409 URLShorten",
+			handler:  "URLShorten",
+			endpoint: "/",
+			body:     "https://practicum.yandex.ru/learn/go-advanced/courses/14d6ff29-c8b6-43bf-9c55-12e8fe25b1b0/sprints/39172/topics/add19e4a-79bf-416e-9d13-0df2005ec81e/lessons/4280c1ec-45c9-4b73-bdf1-8ea438b18212/",
+			method:   http.MethodPost,
+			want: want{
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name:     "success 409 URLShortenJSON",
+			handler:  "URLShortenJSON",
+			endpoint: "/api/shorten",
+			body:     `{"url":"https://practicum.yandex.ru/learn/go-advanced/courses/14d6ff29-c8b6-43bf-9c55-12e8fe25b1b0/sprints/39172/topics/add19e4a-79bf-416e-9d13-0df2005ec81e/lessons/4280c1ec-45c9-4b73-bdf1-8ea438b18212/"}`,
+			method:   http.MethodPost,
+			want: want{
+				contentType: "application/json",
+			},
+		},
+	}
+	for _, tt := range tests {
+		database.ManageDB()
+		t.Run(tt.name, func(t *testing.T) {
+			var body = []byte(tt.body)
+			request1 := httptest.NewRequest(
+				tt.method,
+				tt.endpoint,
+				bytes.NewBuffer(body),
+			)
+
+			server := Server{
+				DBStorage: &url.DBStorage{
+					DB: database.DB,
+				},
+			}
+			var handler func(w http.ResponseWriter, r *http.Request)
+			if tt.handler == "URLShorten" {
+				handler = server.URLShorten
+			} else {
+				handler = server.URLShortenJSON
+			}
+			defer func() {
+				_, err := server.DBStorage.DB.Conn.Exec(q.DropAll)
+				assert.NoError(t, err)
+			}()
+
+			router1 := mux.NewRouter()
+			router1.HandleFunc(tt.endpoint,
+				handler).Methods("POST")
+			writer1 := httptest.NewRecorder()
+			router1.ServeHTTP(writer1, request1)
+
+			response1 := writer1.Result()
+			assert.Equal(t, response1.StatusCode, http.StatusCreated)
+
+			request2 := httptest.NewRequest(
+				tt.method,
+				tt.endpoint,
+				bytes.NewBuffer(body),
+			)
+			router2 := mux.NewRouter()
+			router2.HandleFunc(tt.endpoint,
+				handler).Methods("POST")
+			writer2 := httptest.NewRecorder()
+			router2.ServeHTTP(writer2, request2)
+
+			response2 := writer2.Result()
+			assert.Equal(t, response2.StatusCode, http.StatusConflict)
+			assert.Equal(t, tt.want.contentType, response2.Header.Get("Content-Type"))
+
+			err := response1.Body.Close()
+			require.NoError(t, err)
+			err = response2.Body.Close()
+			require.NoError(t, err)
+		})
+	}
+}
