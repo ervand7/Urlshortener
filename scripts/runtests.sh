bb9204406017e6366e2caec120faeb5ec1c2f4ab
ervand7 <ervand7@mail.ru>
2022-12-12 23:24:33 +0400

    refactoring scripts/runtests.sh

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -7,13 +7,13 @@ go test \
   github.com/ervand7/urlshortener/internal/controllers/generatedata \
   github.com/ervand7/urlshortener/internal/server \
   github.com/ervand7/urlshortener/internal/views/ \
-  -count 1 -v
+  -count 1 -v -p 1
 
 go test github.com/ervand7/urlshortener/internal/controllers/storage/ \
-  -count 1 -v
+  -count 1 -v -p 1
 
 export DATABASE_DSN='user=ervand password=ervand dbname=urlshortener_test host=localhost port=5432 sslmode=disable'
 go test github.com/ervand7/urlshortener/internal/controllers/storage/ \
-  -count 1 -v
+  -count 1 -v -p 1
 go test github.com/ervand7/urlshortener/internal/views/ \
-  -count 1 -v
+  -count 1 -v -p 1
