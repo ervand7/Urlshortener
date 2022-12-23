fe21f4c3b5be6af29d57b56d09819ed87d13a3d6
ervand7 <ervand7@mail.ru>
2022-12-23 11:10:56 +0400

    temp

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"github.com/ervand7/urlshortener/internal/logger"
 	"github.com/ervand7/urlshortener/internal/server"
+	"log"
+	"net/http"
+	//_ "net/http/pprof"
 )
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe(":6060", nil))
+	}()
+
 	logger.Logger.Info("server started")
 	server.Run()
 }
